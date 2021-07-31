a04150626e07d7005c690c453142fc92d62ca9c1
tbsnfruits <llinoa22@gmail.com>
2021-08-01 01:02:57 +0900

    merge with cnp file

--- musical/app.py
+++ musical/app.py
@@ -7,7 +7,7 @@ db = client.dbsparta
 
 @app.route('/')
 def home():
-    return render_template('index-1.html')
+    return render_template('index.html')
 
 @app.route('/musicals', methods=['GET'])
 def read_musicals():
